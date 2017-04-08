0da5be30e7a7859fb899b886cd25e18ceb34d463
Jeff <jeffyokup@gmail.com>
2017-04-08 02:19:56 -0500

    Fucking adding some start functionality bitches
    Useful descritpinot bitch
    lulz jk lulz

---
+++ Assets/StartButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour {
+
+	//lol go away 
+    public void changeScene(int index)
+    {
+        Application.LoadLevel(index);
+    }
+}
