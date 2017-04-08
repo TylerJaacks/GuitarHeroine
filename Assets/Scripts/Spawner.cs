25a412edfe668aef9c3c71fd5b60c8519ece45fd
Tyler Jaacks <TylerJaacks@Gmail.com>
2017-04-08 03:29:11 -0500

    Working on Randomly Spawning Notes

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,11 +2,19 @@
 
 public class Spawner : MonoBehaviour 
 {
+	public MusicManager musicManager;
 	public Note blueNote;
 	public Note greenNote;
 	public Note redNote;
 	public Note yellowNote;
 	public GameObject fretBoard;
+	public float delay = 1.5f;
+	public bool shouldSpawn = true;
+
+	public int blue = 0;
+	public int green = 1; 
+	public int red = 2;
+	public int yellow = 3;
 
 	Vector3 blueSpanwPos = new Vector3(-2.0f, 4.65f, 7.58f);
 	Vector3 greenSpawnPos = new Vector3(-0.7f, 4.65f, 7.58f);
@@ -15,15 +23,43 @@ public class Spawner : MonoBehaviour
 
 	void Start () 
 	{
-		GameObject.Instantiate(blueNote, blueSpanwPos, new Quaternion(0, 0, 0, 0), fretBoard.transform);
-		GameObject.Instantiate(greenNote, greenSpawnPos, new Quaternion(0, 0, 0, 0), fretBoard.transform);
-		GameObject.Instantiate(redNote, redNoteSpawnPos, new Quaternion(0, 0, 0, 0), fretBoard.transform);
-		GameObject.Instantiate(yellowNote, yellowNoteSpawnPos, new Quaternion(0, 0, 0, 0), fretBoard.transform);
-
+		musicManager = new MusicManager();
 	}
 
 	void Update () 
 	{
-		
+		if (musicManager.songIsOver)
+		{
+			shouldSpawn = false;
+		}
+
+		while (shouldSpawn) 
+		{
+			System.Random random = new System.Random();
+			Spawn(random.Next(0, 3));
+		}
+	}
+
+	void Spawn(int note) 
+	{
+		if (note == 0)
+		{
+			GameObject.Instantiate(blueNote, blueSpanwPos, new Quaternion(0, 0, 0, 0), fretBoard.transform);
+		}
+
+		if (note == 1)
+		{
+			GameObject.Instantiate(greenNote, greenSpawnPos, new Quaternion(0, 0, 0, 0), fretBoard.transform);
+		}
+
+		if (note == 2)
+		{
+			GameObject.Instantiate(redNote, redNoteSpawnPos, new Quaternion(0, 0, 0, 0), fretBoard.transform);
+		}
+
+		if (note == 3)
+		{
+			GameObject.Instantiate(yellowNote, yellowNoteSpawnPos, new Quaternion(0, 0, 0, 0), fretBoard.transform);
+		}
 	}
 }
