cfc07525f95703dfc637ad0e89def63ba936c32c
Tyler Jaacks <TylerJaacks@Gmail.com>
2017-04-08 01:13:07 -0500

    Stuff
    Stuff

--- Assets/Scripts/TriggerDetection.cs
+++ Assets/Scripts/TriggerDetection.cs
@@ -10,6 +10,8 @@ public class TriggerDetection : MonoBehaviour
 		if (Input.GetButton(buttonName) && note != null)
 		{
 			// Do Stuff
+			// Update Score
+			// And Add Animation-thingy.
 
 			Destroy(note.gameObject);
 			note = null;
