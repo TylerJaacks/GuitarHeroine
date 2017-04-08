7a566f7a5518abda5520a56fffaf208b07f9dab2
Tyler Jaacks <TylerJaacks@Gmail.com>
2017-04-07 23:54:45 -0500

    Added full inital spawn

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,20 +4,26 @@ public class Spawner : MonoBehaviour
 {
 	public Note blueNote;
 	public Note greenNote;
+	public Note redNote;
+	public Note yellowNote;
 	public GameObject fretBoard;
 
 	Vector3 blueSpanwPos = new Vector3(-2.0f, 4.65f, 7.58f);
-	Vector3 greenSpawnPos = new Vector3(-2.7f, 4.65f, 7.58f);
-
+	Vector3 greenSpawnPos = new Vector3(-0.7f, 4.65f, 7.58f);
+	Vector3 redNoteSpawnPos = new Vector3(0.69f, 4.65f, 7.58f);
+	Vector3 yellowNoteSpawnPos = new Vector3(2.0f, 4.65f, 7.58f);
 
 	void Start () 
 	{
 		GameObject.Instantiate(blueNote, blueSpanwPos, new Quaternion(0, 0, 0, 0), fretBoard.transform);
 		GameObject.Instantiate(greenNote, greenSpawnPos, new Quaternion(0, 0, 0, 0), fretBoard.transform);
+		GameObject.Instantiate(redNote, redNoteSpawnPos, new Quaternion(0, 0, 0, 0), fretBoard.transform);
+		GameObject.Instantiate(yellowNote, yellowNoteSpawnPos, new Quaternion(0, 0, 0, 0), fretBoard.transform);
+
 	}
 
 	void Update () 
 	{
-		
+		// Check if the note makes contact with the note buttons.
 	}
 }
